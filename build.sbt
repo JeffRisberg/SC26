995c70bec2a0d9363bee1b34da6a574f512d2bfe
JeffRisberg <jrisberg@aol.com>
2015-02-11 17:24:55 -0800

    added use of akka

--- build.sbt
+++ build.sbt
@@ -1,4 +1,6 @@
 name := "SC26"
 
 version := "1.0"
-    
+
+libraryDependencies +=
+  "com.typesafe.akka" %% "akka-actor" % "2.4-SNAPSHOT"
---
+++ src/main/scala/com/incra/main/ShapeDrawingActor/scala.scala
@@ -0,0 +1,30 @@
+package com.incra.main.ShapeDrawingActor
+
+/**
+ * Created by jeff on 2/11/15.
+ */
+object Messages {
+  object Exit
+  object Finished
+  case class Response(message: String)
+}
+
+import akka.actor.Actor
+import com.incra.models.Shape
+
+class ShapesDrawingActor extends Actor {
+  import Messages._
+
+  def receive = {
+    case s: Shape =>
+      s.draw(str => println(s"ShapesDrawingActor: $str"))
+      sender ! Response(s"shapesDrawingActive: $s drawn")
+    case Exit =>
+      println(s"ShapeDrawingActor: exiting")
+      sender ! Finished
+    case unexpected =>
+      val response = Response(s"Error Unknown message $unexpected")
+      println(s"ShapesDrawingActor $response")
+      sender ! response
+  }
+}
